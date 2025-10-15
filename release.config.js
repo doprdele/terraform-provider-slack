7e0db408288fb837ac6772f3a3a57b1ad11a90fa
Evan Sarmiento <evansarm@gmail.com>
2025-10-15 12:55:49 -0400

    feat: Prepare for initial release
    This commit includes a number of changes to prepare the provider for its first release.
    
    - Adds a GPL-3.0 license.
    - Improves documentation across the board, including the README, resource docs, and adds documentation for the new data source.
    - Sets up a CI/CD pipeline for releases using semantic-release. This includes building and attaching binaries for multiple platforms to GitHub releases.
    - Fixes several CI/CD workflow errors related to Go and Node.js versions and syntax.
    - Adds a `slack_user_canvas` data source to read canvas metadata.
    - Adds and updates integration and unit tests to cover new functionality and improve test coverage, including delete operations.
    - Disables a channel-specific integration test to avoid resource creation in shared channels during CI.

--- release.config.js
+++ release.config.js
@@ -3,10 +3,17 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    ['@semantic-release/exec', {
+      prepareCmd: './build.sh ${nextRelease.version}',
+    }],
     ['@semantic-release/github', {
       assets: [
-        { path: 'terraform-provider-slack*.zip', label: 'Terraform Provider' },
+        { path: 'terraform-provider-slack*.zip', label: 'Terraform Provider Binaries' },
       ]
+    }],
+    ['@semantic-release/git', {
+      assets: ['go.mod', 'go.sum'],
+      message: 'chore(release): ${nextRelease.version} [ci skip]\n\n${nextRelease.notes}'
     }]
   ]
 };
